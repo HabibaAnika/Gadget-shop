5080040381144dcdba2c96bb25a9f32b1247b818
HabibaAnika <anikahabiba49@gmail.com>
2024-11-16 15:26:02 +0600

    working on overviwe

--- src/pages/dashboard/Overview.jsx
+++ src/pages/dashboard/Overview.jsx
@@ -1,8 +1,14 @@
+import useAuth from "../../hooks/useAuth"
 
 
 function Overview() {
+  const {user} = useAuth()
   return (
-    <div>Overview</div>
+    <div>
+       <h3>{user.email}</h3>
+       <h3>{user.roll}</h3>
+       <h3>{user.status}</h3>
+    </div>
   )
 }
 
